7a5da120530f91f3a72e6157d04c0c7045794900
Leonardo Pontes <leopontes.1989@gmail.com>
2023-03-30 00:57:03 -0300

    exercises finished

--- fundamentos/secao-03-introducao-javascript/dia-2-array-loop-for/scripts.js
+++ fundamentos/secao-03-introducao-javascript/dia-2-array-loop-for/scripts.js
@@ -1,52 +1,38 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let count = 0;
-let maiorNumero = 0;
-let numerosImpares = 0;
-let menorNumero = 5000;
+let n = 10;
 
-for (i=0; i<numbers.length; i++) {
-    console.log(numbers[i])
-    count += numbers[i];
-    if (numbers[i] > maiorNumero) {
-        maiorNumero = numbers[i];
+for (let i=n-1; i > 0; i--) {
+    n *= i;  
+}
+
+console.log(n);
+
+//
+let word = 'tryber';
+let inverter = 'Invertendo palavras: ';
+for (i=word.length; i>=0; i--) {
+    console.log(word[i])
+    inverter += word[i];
+}
+
+console.log(inverter);
+
+//
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maisCaracteres = '';
+let menosCaracteres = '';
+
+for (i=0; i<array.length; i++) {
+    if (array[i].length > maisCaracteres.length) {
+        maisCaracteres = array[i];
     }
-    if (numbers[i] % 2 !== 0) {
-        numerosImpares++;
+    if (array[i]) {
+        menosCaracteres = array[i]
     }
-    if (numbers[i] < menorNumero) {
-        menorNumero = numbers[i];
+    if (array[i].length < menosCaracteres.length) {
+        menosCaracteres = array[i];
     }
 }
 
-console.log(count);
-
-media = count / numbers.length;
-console.log('Média aritmética: ', media);
-
-if(media > 20) {
-    console.log('Valor maior que 20!')
-} else {
-    console.log('Valor menor ou igual que 20!');
-}
-
-console.log('Maior número do array é: ', maiorNumero);
-
-if (numerosImpares === 0) {
-    console.log('Nenhum número ímpar encontrado!');
-} else {
-    console.log('Números ímpares: ', numerosImpares);
-}
-
-console.log('Menor número do array: ', menorNumero);
-
-let arrayNumeros = [];
-
-for (i = 1; i <= 25; i++) {
-    arrayNumeros.push(i);
-}
-
-console.log('Array de números até 25: ', arrayNumeros);
-
-for (i=0; i<arrayNumeros.length; i++) {
-    console.log(arrayNumeros[i] / 2);
-}
+console.log('Palavra com mais caracteres: ', maisCaracteres);
+console.log('Palavra com menos caracteres: ', menosCaracteres);
