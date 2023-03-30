423b725a844be835b34f6429434060c338e58972
Leonardo Pontes <leopontes.1989@gmail.com>
2023-03-30 16:19:56 -0300

    exercises finished

--- fundamentos/secao-03-introducao-javascript/dia-2-array-loop-for/scripts.js
+++ fundamentos/secao-03-introducao-javascript/dia-2-array-loop-for/scripts.js
@@ -1,4 +1,4 @@
-let n = 10;
+/* let n = 10;
 
 for (let i=n-1; i > 0; i--) {
     n *= i;  
@@ -33,6 +33,75 @@ for (i=0; i<array.length; i++) {
         menosCaracteres = array[i];
     }
 }
-
-console.log('Palavra com mais caracteres: ', maisCaracteres);
+ */
+/* console.log('Palavra com mais caracteres: ', maisCaracteres);
 console.log('Palavra com menos caracteres: ', menosCaracteres);
+ */
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+/* for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] < numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
+  }
+}
+
+for (i=0; i<numbers.length; i++) {
+    console.log(numbers[i])
+} */
+
+/* console.log('Vetor em ordem crescente: ', numbers); */
+
+/* for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] > numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
+  }
+}
+
+for (i=0; i<numbers.length; i++) {
+    console.log(numbers[i])
+}
+
+console.log('Vetor em ordem decrescente: ', numbers); */
+
+
+/* let numbersMultiply = [];
+
+for (let i = 0; i < 1; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+        if (j < numbers.length - 1) {
+            numbersMultiply.push(numbers[j] * numbers[j + 1]);
+        }
+        else {
+            numbersMultiply.push(numbers[j] * 2);
+        }
+    }
+}
+console.log(numbers);
+console.log(numbersMultiply); */
+
+
+for (let i = 0; i < 5; i++) {
+    console.log('*****');
+    for (let j = 0; j < 5; j++) {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
