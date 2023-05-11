d9d0bcbe99dfd46b48515d0c693d79f1ec36fc4d
Leonardo Pontes <leopontes.1989@gmail.com>
2023-05-11 15:58:40 -0300

    exercise done

---
+++ fundamentos/secao-07-hof/filter-reduce/script.js
@@ -0,0 +1,97 @@
+// Fonte: <https://restcountries.com/v2/all>
+const countries = [
+  {
+    name: 'Afghanistan',
+    region: 'Asia',
+    currencies: [{ code: 'AFN', name: 'Afghan afghani' }],
+    capital: 'Kabul',
+    population: 40218234,
+    area: 652230
+  },
+  {
+    name: 'Åland Islands',
+    region: 'Europe',
+    currencies: [{ code: 'EUR', name: 'Euro' }],
+    capital: 'Mariehamn',
+    population: 28875,
+    area: 1580
+  },
+  {
+    name: 'Albania',
+    region: 'Europe',
+    currencies: [{ code: 'ALL', name: 'Albanian lek' }],
+    capital: 'Tirana',
+    population: 2837743,
+    area: 28748
+  },
+  {
+    name: 'Algeria',
+    region: 'Africa',
+    currencies: [{ code: 'DZD', name: 'Algerian dinar' }],
+    capital: 'Algiers',
+    population: 44700000,
+    area: 2381741
+  },
+  {
+    name: 'American Samoa',
+    region: 'Oceania',
+    currencies: [{ code: 'USD', name: 'United States Dollar' }],
+    capital: 'Pago Pago',
+    population: 55197,
+    area: 199
+  },
+  {
+    name: 'Andorra',
+    region: 'Europe',
+    currencies: [{ code: 'EUR', name: 'Euro' }],
+    capital: 'Andorra la Vella',
+    population: 77265,
+    area: 468
+  },
+  {
+    name: 'Angola',
+    region: 'Africa',
+    currencies: [{ code: 'AOA', name: 'Angolan kwanza' }],
+    capital: 'Luanda',
+    population: 32866268,
+    area: 1246700
+  },
+  {
+    name: 'Anguilla',
+    region: 'Americas',
+    currencies: [{ code: 'XCD', name: 'East Caribbean dollar' }],
+    capital: 'The Valley',
+    population: 13452,
+    area: 91
+  }
+];
+const totalPopulation = countries.map((element) => element.population);
+
+const totalArea = countries.map((element) => element.area);
+
+const getPopulation = () => totalPopulation.reduce((a, b) => a + b);
+
+const getTotalArea = () => totalArea.reduce((a, b) => a + b);
+
+const longestName = () => countries.reduce((a, b) => (a.name.length > b.name.length) ? a : b);
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+const strNames = names.join().toLowerCase();
+const arrayLetter = strNames.split('');
+
+const countA = () => arrayLetter
+  .reduce((acumulado, atual) => atual === 'a' ? acumulado +=1 : acumulado, 0);
+
+// O index 0 do array `students` equivale ao index 0 do array `grades`
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+const averageArray = grades.map((grade) => grade.reduce((a, b) => (a + b)) / 5);
+
+const studentAverage = () => students.map((student, index) => 
+  `{ name: ${student}, average: ${averageArray[index]} }`);
