f2a652b4c99e57f73ee2e98cfbe9cb88352d5858
Leonardo Pontes <leopontes.1989@gmail.com>
2023-05-11 22:31:09 -0300

    first commit

---
+++ fundamentos/secao-07-hof/exercise2.js
@@ -0,0 +1,2 @@
+const sum = (...args) => args.reduce((acc, curr) => acc + curr, 0);
+console.log(sum(1,2,3,4,5,6,7,8,10));
---
+++ fundamentos/secao-07-hof/testes.js
@@ -0,0 +1,57 @@
+const primeNumbers = [17, 23, 37]
+
+const sum = (a, b) => {
+  console.log(a + b);
+}
+
+sum(primeNumbers[0], primeNumbers[2]) // 54
+
+const [first, second, third] = primeNumbers;
+
+sum(first, third);
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+[comida, animal, bebida] = [bebida, comida, animal];
+
+console.log(comida, animal, bebida); // arroz gato água
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+[, ...numerosPares] = numerosPares;
+
+console.log(numerosPares);
+
+const getNationality = ({ firstName, nationality = 'Brazilian' }) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
+
+const getPosition = (latitude, longitude) => ({
+  latitude,
+  longitude,
+});
+
+console.log(getPosition(-19.8157, -43.9542));
+
+
+const multiply = (number, value = 1) => {
+  return number * value;
+};
+
+console.log(multiply(8));
